50a574decba759c7a96b1d50279e5bd51cd40017
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2021-07-01 11:43:00 +0200

    Fix #1: Only show edit action for supported mimetypes

--- js/script.js
+++ js/script.js
@@ -2,17 +2,55 @@
 
 	var FilesLibreOfficeEditPlugin = {
 		attach: function (fileList) {
-			var self = this
+			var self = this;
 
-			fileList.fileActions.registerAction({
-				name: 'EditWithLibreOffice',
-				displayName: t('fileslibreofficeedit', 'Edit with LibreOffice'),
-				mime: 'all',
-				order: 100,
-				iconClass: 'icon-edit',
-				permissions: OC.PERMISSION_UPDATE,
-				actionHandler: self.editWithLO
-			})
+			var supportedMimetypes = [
+				"text/plain",
+
+				"application/vnd.oasis.opendocument.formula",
+				"application/vnd.oasis.opendocument.graphics",
+				"application/vnd.oasis.opendocument.presentation",
+				"application/vnd.oasis.opendocument.spreadsheet",
+				"application/vnd.oasis.opendocument.text",
+				"application/vnd.oasis.opendocument.text-template",
+				"application/vnd.oasis.opendocument.spreadsheet-template",
+				"application/vnd.oasis.opendocument.presentation-template",
+				"application/vnd.oasis.opendocument.graphics-template",
+
+				"application/msword",
+				"application/vnd.ms-word.document.macroEnabled.12",
+				"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+				"application/vnd.openxmlformats-officedocument.wordprocessingml.template",
+
+				"application/vnd.ms-excel",
+				"application/vnd.ms-excel.addin.macroEnabled.12",
+				"application/vnd.ms-excel.sheet.binary.macroEnabled.12",
+				"application/vnd.ms-excel.sheet.macroEnabled.12",
+				"application/vnd.ms-excel.template.macroEnabled.12",
+				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+				"application/vnd.openxmlformats-officedocument.spreadsheetml.template",
+
+				"application/vnd.ms-powerpoint",
+				"application/vnd.ms-powerpoint.template.macroEnabled.12",
+				"application/vnd.ms-powerpoint.addin.macroEnabled.12",
+				"application/vnd.ms-powerpoint.slideshow.macroEnabled.12",
+				"application/vnd.ms-powerpoint.presentation.macroEnabled.12",
+				"application/vnd.openxmlformats-officedocument.presentationml.presentation",
+				"application/vnd.openxmlformats-officedocument.presentationml.slideshow",
+				"application/vnd.openxmlformats-officedocument.presentationml.template",
+			];
+
+			supportedMimetypes.forEach (function (mimetype) {
+				fileList.fileActions.registerAction({
+					name: 'EditWithLibreOffice',
+					displayName: t('fileslibreofficeedit', 'Edit with LibreOffice'),
+					mime: mimetype,
+					order: 100,
+					iconClass: 'icon-edit',
+					permissions: OC.PERMISSION_UPDATE,
+					actionHandler: self.editWithLO
+				})
+			});
 		},
 
 		editWithLO: function (fileName, context) {
